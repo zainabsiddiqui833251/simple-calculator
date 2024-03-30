7c61f989330334779682fbf343fb766a1d481aaf
Zainab Siddiqui <zs833251@gmail.com>
2024-03-30 17:07:27 +0500

    second commit

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
+#!/usr/bin/env node
 import inquirer from "inquirer";
-const answer = await inquirer.prompt([
-    { message: "Enter first number", type: "number", name: "firstNumber" },
-    { message: "Enter second number", type: "number", name: "secondNumber" },
-    {
-        message: "select one of the operators to perform operation",
-        type: "list",
-        name: "operators",
-        choices: ["addition", "subtraction", "multiplication", "division"],
+async function main() {
+    const answer = await inquirer.prompt([
+        { message: "Enter first number", type: "number", name: "firstNumber" },
+        { message: "Enter second number", type: "number", name: "secondNumber" },
+        {
+            message: "select one of the operators to perform operation",
+            type: "list",
+            name: "operators",
+            choices: ["addition", "subtraction", "multiplication", "division"],
+        }
+    ]);
+    //conditional statements
+    if (answer.operators === "addition") {
+        console.log(answer.firstNumber + answer.secondNumber);
     }
-]);
-//conditional statements
-if (answer.operators === "addition") {
-    console.log(answer.firstNumber + answer.secondNumber);
+    else if (answer.operators === "subtraction") {
+        console.log(answer.firstNumber - answer.secondNumber);
+    }
+    else if (answer.operators === "multiplication") {
+        console.log(answer.firstNumber * answer.secondNumber);
+    }
+    else if (answer.operators === "division") {
+        console.log(answer.firstNumber / answer.secondNumber);
+    }
+    else {
+        console.log("Select a valid operator");
+    }
 }
-else if (answer.operators === "subtraction") {
-    console.log(answer.firstNumber - answer.secondNumber);
-}
-else if (answer.operators === "multiplication") {
-    console.log(answer.firstNumber * answer.secondNumber);
-}
-else if (answer.operators === "division") {
-    console.log(answer.firstNumber / answer.secondNumber);
-}
-else {
-    console.log("Select a valid operator");
-}
+main();
